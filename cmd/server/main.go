7bb28f0ba9328572ab9d1b6c2bfa7ca742124c2a
Lucas Carturani <lucascarturani@gmail.com>
2026-02-01 22:56:57 -0300

    first commit from my api

---
+++ cmd/server/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/Lukaoxp/status-monitor/internal/health"
+)
+
+func main() {
+	http.HandleFunc("/status", healthHandler)
+
+	port := getEnv("PORT", "8080")
+
+	log.Printf("Server starting on :%s...", port)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getEnv(key, fallback string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return fallback
+	}
+	return value
+}
+
+func healthHandler(w http.ResponseWriter, _ *http.Request) {
+
+	data := health.GetStatus()
+	response, err := json.Marshal(data)
+	if err != nil {
+		log.Printf("error marshalling health status: %v", err)
+		fmt.Println(err.Error())
+
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
+}
