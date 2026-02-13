e221116152f3c7c4b4888aa4a5fd072aa7b8fec5
Lucas Carturani <lucascarturani@gmail.com>
2026-02-13 18:11:47 -0300

    inicio da go routine

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/Lukaoxp/status-monitor/internal/health"
 )
 
 const APIVersion = "1.0.0"
 
+// Fluxo principal:
+// - Inicializa o serviço de health com versionamento
+// - Cria o handler HTTP injetando dependências
+// - Lê a porta da variável de ambiente (com fallback)
+// - Inicia o servidor HTTP
 func main() {
+
 	newService := health.NewService(APIVersion)
 	server := &Server{healthService: newService}
 
@@ -21,40 +24,15 @@ func main() {
 	port := getEnv("PORT", "8080")
 
 	log.Printf("Server starting on localhost:%s...", port)
+
+	// srv := http.Server{
+	// 	Addr:    ":" + port,
+	// 	Handler: nil,
+	// }
+
 	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-}
 
-func getEnv(key, fallback string) string {
-	value := os.Getenv(key)
-	if value == "" {
-		return fallback
-	}
-	return value
-}
-
-// 1. Definimos a struct que guardará nossas dependências
-type Server struct {
-	healthService *health.Service
-}
-
-// 2. O (s *Server) é o receiver. Ele diz que esta função pertence ao Server.
-// Agora podemos usar 's.healthService' aqui dentro sem criá-lo do zero!
-func (s *Server) healthHandler(w http.ResponseWriter, _ *http.Request) {
-
-	// service := health.NewService(APIVersion)
-	data := s.healthService.GetStatus()
-	response, err := json.Marshal(data)
-	if err != nil {
-		log.Printf("error marshalling health status: %v", err)
-		fmt.Println(err.Error())
-
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(response)
 }
